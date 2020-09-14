f6c98782a51e6cc0e5fb8c9afa9a10edb4ead63a
llaugustoll <53905902+llaugustoll@users.noreply.github.com>
2020-09-14 15:20:49 -0300

    Exercicio 2 Completto
    Verificar a posição e mostrar ID

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package view;
+
+import controller.ThreadId;
+
+public class Principal {
+ public static void main(String[] args) {
+	for (int idThread = 0 ; idThread <5 ; idThread++){
+		Thread threadId = new ThreadId(idThread);
+		threadId.start();
+	}
+}
+}
